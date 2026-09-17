49506fe275334d70898501752c90a054a090fc33
rbokka-sre <167917226+rbokka-sre@users.noreply.github.com>
2026-09-17 12:29:04 +0530

    Update resource.tf

--- day3/resource.tf
+++ day3/resource.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "my-new-webapp" {
     Environment = var.env
     }
   ami = var.ami
-  instance_type = var.instance
+  instance_type = var.instance_type
   availability_zone = var.availability_zone
   key_name = var.key_name
   root_block_device {
