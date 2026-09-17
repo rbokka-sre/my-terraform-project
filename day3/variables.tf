2221dbc63817288b25cbaad3cbcfbd90b2ea8abd
rbokka-sre <167917226+rbokka-sre@users.noreply.github.com>
2026-09-17 12:14:26 +0530

    Add Terraform variables for environment configuration

---
+++ day3/variables.tf
@@ -0,0 +1,24 @@
+variable env {
+  type = string
+  default = "stage"
+}
+variable ami {
+  type = string
+  default = "ami-01a00762f46d584a1"
+}
+variable instance_type {
+  type = string
+  default = "t3.micro"
+}
+variable availability_zone {
+  type = string
+  default = "ap-south-1a"
+}
+variable key_name {
+  type = string
+  default = "jump-box-1"
+}
+variable voulume_size {
+  type = string
+  default = "8"
+}
