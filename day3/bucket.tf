ca5328972ab887ecbb8826932add27a6d991e0cb
rbokka-sre <167917226+rbokka-sre@users.noreply.github.com>
2026-09-17 12:46:38 +0530

    Add S3 bucket and versioning configuration

---
+++ day3/bucket.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "web-tfstate-bucket" {
+bucket = "web-statefile-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "web-tfstate-bucket-versioning" {
+bucket = aws_s3_bucket.web-tfstate-bucket.id
+
+versioning_configuration {
+status = Enabled
+}
+}
